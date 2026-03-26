b2fcc98ca2e006b9631cc85b6e51cc97b558996b
choi yujin <jinye24@naver.com>
2026-03-26 15:29:49 +0900

    26.03.26(목) 10일차 - js

---
+++ ex10-09b.js
@@ -0,0 +1,6 @@
+function fn(...arr) {
+  // 나머지 매개변수 ; py: (*args)
+  console.log(arr, typeof arr, Array.isArray(arr));
+  console.log(...arr); // 전개연산자 ; py: fn[:]
+}
+fn(1, 2, 3, 4, 5, 6);
---
+++ ex10-09d.js
@@ -0,0 +1,24 @@
+/* object, 객체 다루기 */
+
+const user = { name: "홍길동", age: 10 };
+// 객체의 키로 값을 읽는 법 - R
+console.log(user.name);
+console.log(user["name"]);
+// 객체에 키값쌍을 추가하는 법 - C
+const 변수 = "address";
+user.gender = "male";
+user[변수] = "대한민국 부산광역시";
+// 키값쌍을 삭제하는 법 - D
+delete user.gender;
+delete user[변수];
+delete user["age"];
+console.log(user);
+
+/* Tip */
+const ok = "학교";
+const test = { ok }; // {ok : ok}
+console.log(test);
+
+const obj = { t1: 11, t2: 22, t3: 33, t4: 44 };
+const updated = { ...obj, t4: 44 };
+console.log(updated);
